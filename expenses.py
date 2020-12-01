f41cc807debabc2bdffc666556e37cbe1a694530
Carolinecah <caroline.cah@polestar.com>
2020-12-01 22:23:14 +0100

    Progress in one line solution, part 1

--- expenses.py
+++ expenses.py
@@ -9,3 +9,5 @@ for x in numbers:
         for z in numbers:
             if x+y+z == 2020 and len(set([numbers.index(x), numbers.index(y), numbers.index(z)])) == 3:
                 print('Part two: {}'.format(x*y*z))
+
+                print(reduce(lambda j,i: j*i, [j for j in numbers if (2020-j) in set(numbers)]))
