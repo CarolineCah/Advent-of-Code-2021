a722e49cc268f27bcf202c95bdb6cc8fd21550b5
Carolinecah <caroline.cah@polestar.com>
2020-12-01 21:37:01 +0100

    Day 1, solution

---
+++ expenses.py
@@ -0,0 +1,11 @@
+numbers = map(int, open("numbers.txt", 'r').readlines())
+print(len(numbers))
+
+for x in numbers:
+    for y in numbers:
+        if x+y == 2020 and len(set([numbers.index(x), numbers.index(y)])) == 2:
+            print('Part one: {}'.format(x*y))
+           
+        for z in numbers:
+            if x+y+z == 2020 and len(set([numbers.index(x), numbers.index(y), numbers.index(z)])) == 3:
+                print('Part two: {}'.format(x*y*z))
